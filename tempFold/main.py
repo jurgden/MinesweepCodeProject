8b32ede3de2b83d73b0e094a3d57ce7734abaea9
jurgden <82841924+jurgden@users.noreply.github.com>
2022-07-18 11:12:24 -0700

    Basic software scaffolding with Tkinter

---
+++ tempFold/main.py
@@ -0,0 +1,7 @@
+from tkinter import *
+
+# below out "root" and above our "mainloop" is the core internals code of the program
+root = Tk()
+root.geometry('1472x720')
+root.title("Minesweeper Projx")
+root.mainloop()
