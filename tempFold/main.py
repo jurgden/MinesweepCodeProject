7628e24cb0b686a163b2f9f7a43ca98eebec895c
jurgden <82841924+jurgden@users.noreply.github.com>
2022-07-18 11:15:44 -0700

    changed resizing to false

--- tempFold/main.py
+++ tempFold/main.py
@@ -2,6 +2,10 @@ from tkinter import *
 
 # below out "root" and above our "mainloop" is the core internals code of the program
 root = Tk()
+# window size/resolution
 root.geometry('1472x720')
+# our top left software window title
 root.title("Minesweeper Projx")
+# this controls whether or not the user can resize the window (currently false)
+root.resizable(False, False)
 root.mainloop()
