06388aed27f739b10b6f2776c403fc01ce10be6f
jurgden <82841924+jurgden@users.noreply.github.com>
2022-07-18 19:53:54 -0700

    added folder for cut content

--- tempFold/main.py
+++ tempFold/main.py
@@ -42,32 +42,8 @@ center_frame = Frame(
 
 center_frame.place(
     x=utils.width_prct(25),
-    y=utils.height_prct(25),
+    y=utils.height_prct(25)
 )
-
-c1 = Cell()
-c1.create_btn_object(center_frame)
-c1.cell_btn_object.grid(
-    column=0, row=1
-)
-
-c2 = Cell()
-c2.create_btn_object(center_frame)
-c2.cell_btn_object.grid(
-    column=0, row=0
-)
-
-# btn1 = Button(
-# center_frame,
-# bg='#8CD790',
-# text='First Button',
-# bd=5,
-# fg='#54426B',
-# relief=GROOVE,
-# activebackground='#449DD1')
-
-# btn1.place(x=utils.width_prct(25), y=utils.height_prct(25))
-
 
 # Runs the window
 root.mainloop()
